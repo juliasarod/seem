29c7ba9568ce64cd93f69c9b57ff9ad23ebf57fd
juliasarod <juliasantosr47@gmail.com>
2026-04-23 22:15:56 -0300

    fix: corrige bugs na pesquisa e lista de episodios

--- script.js
+++ script.js
@@ -34,14 +34,15 @@ function continuar(event) {
 
 };
 
-document.getElementById('loginForm').addEventListener('submit', function(e) {
-    e.preventDefault();
-    
-    const username = document.getElementById('user').value;
-    localStorage.setItem('nomeUsuario', username);
-    
-    window.location.href = 'catalogo.html';
-});
+const loginForm = document.getElementById('loginForm');
+if (loginForm) {
+    loginForm.addEventListener('submit', function(e) {
+        e.preventDefault();
+        const username = document.getElementById('user').value;
+        localStorage.setItem('nomeUsuario', username);
+        window.location.href = 'catalogo.html';
+    });
+}
 
 window.onload = function () {
     const nomeSalvo = localStorage.getItem("nomeUsuario");
@@ -54,14 +55,16 @@ window.onload = function () {
     }
 };
 
-document.getElementById('formPesquisar').addEventListener("submit", function (e) {
-    e.preventDefault();
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById('formPesquisar').addEventListener("submit", function (e) {
+        e.preventDefault();
 
-    const termo = document.getElementById("pesquisar").value.trim().toLowerCase();
+        const termo = document.getElementById("pesquisar").value.trim().toLowerCase();
 
-    if (termo === "it") {
-        window.location.href = "resultados.html";
-    }
+        if (termo === "it") {
+            window.location.href = "resultados.html";
+        }
+    });
 });
 
 function carregarVideo() {
@@ -89,7 +92,7 @@ function carregarEps() {
                 <div class="card-filmeEpisodios">
                     <a href="#">
                         <img src="https://m.media-amazon.com/images/M/MV5BYTdlNzkyYjktOTUyMC00MzM4LTgxMTYtZmRhMzA5YzUyZWZlXkEyXkFqcGc@._V1_QL75_UX328_.jpg"
-                            alt="E1S1" class="filmeRecomendado">
+                            alt="E1S1" class="filmeEpisodios">
                         <div class="info-card">
                             <p class="titulo">The Pilot</p>
                             <p class="ano-classificacao">54 min | S1E1</p>
@@ -99,7 +102,7 @@ function carregarEps() {
                 <div class="card-filmeEpisodios">
                     <a href="#">
                         <img src="https://m.media-amazon.com/images/M/MV5BMzZkMDVhZjYtZGYwNy00NjM2LThmN2MtODdmODRmMTMyODI4XkEyXkFqcGc@._V1_QL75_UX328_.jpg" alt="S1E2"
-                            class="catalogofilme">
+                            class="filmeEpisodios">
                         <div class="info-card">
                             <p class="titulo">The Thing in the Dark</p>
                             <p class="ano-classificacao">1h6min| S1E2</p>
@@ -109,7 +112,7 @@ function carregarEps() {
                 <div class="card-filmeEpisodios">
                     <a href="#">
                         <img src="https://m.media-amazon.com/images/M/MV5BYzMzMmM1YzctMjBhYS00MDZjLWJlZjYtNGViNjdjZWYzZTdlXkEyXkFqcGc@._V1_QL75_UX388_.jpg" alt="S1E3"
-                            class="catalogofilme">
+                            class="filmeEpisodios">
                         <div class="info-card">
                             <p class="titulo">Now You See It</p>
                             <p class="ano-classificacao">58min| S1E3</p>
@@ -119,7 +122,7 @@ function carregarEps() {
                 <div class="card-filmeEpisodios">
                     <a href="#">
                         <img src="https://m.media-amazon.com/images/M/MV5BY2JjNDMzYjEtZDkzNS00ZmQzLWFmZjEtMmMxNjExNzYxNGI0XkEyXkFqcGc@._V1_QL75_UX388_.jpg" alt="S1E4"
-                            class="catalogofilme">
+                            class="filmeEpisodios">
                         <div class="info-card">
                             <p class="titulo">The Great Swirling Apparatus of Our Planet's Function</p>
                             <p class="ano-classificacao">1h02min | S1E4</p>
@@ -129,7 +132,7 @@ function carregarEps() {
                 <div class="card-filmeEpisodios">
                     <a href="#">
                         <img src="https://m.media-amazon.com/images/M/MV5BNTkyNjU1ODgtOTljMS00NWZmLTlkMDEtMTE4NGZjOTQyNWY1XkEyXkFqcGc@._V1_QL75_UX350_.jpg" alt="S1E5"
-                            class="catalogofilme">
+                            class="filmeEpisodios">
                         <div class="info-card">
                             <p class="titulo">29 Neilbolt Street</p>
                             <p class="ano-classificacao">55min| S1E5</p>
@@ -139,7 +142,7 @@ function carregarEps() {
                 <div class="card-filmeEpisodios">
                     <a href="#">
                         <img src="https://m.media-amazon.com/images/M/MV5BZDQzZGM2YjUtYTFiNS00MzZkLTlkYzgtMWQzYzg1OTllMzIwXkEyXkFqcGc@._V1_QL75_UX291_.jpg" alt="S1E6"
-                            class="catalogofilme">
+                            class="filmeEpisodios">
                         <div class="info-card">
                             <p class="titulo">In the Name of the Father</p>
                             <p class="ano-classificacao">56min| S1E6</p>
@@ -149,7 +152,7 @@ function carregarEps() {
                 <div class="card-filmeEpisodios">
                     <a href="#">
                         <img src="https://m.media-amazon.com/images/M/MV5BYmRmMDQ0ZjctZWIzYi00M2VlLWJlNzgtY2Q3NTNlZjE1NTU1XkEyXkFqcGc@._V1_QL75_UX328_.jpg" alt="S1E7"
-                            class="catalogofilme">
+                            class="filmeEpisodios">
                         <div class="info-card">
                             <p class="titulo">The Black Spot</p>
                             <p class="ano-classificacao">1h03min | S1E7</p>
